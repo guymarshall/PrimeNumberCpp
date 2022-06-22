dc7075d4bad3c698b1e754029a4c48d004cf203f
Guy Marshall <guymarshall9@gmail.com>
2022-06-22 22:16:50 +0100

    Save numbers to vector, then save to file

--- PrimeNumber.cpp
+++ PrimeNumber.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <math.h>
 #include "prime.hpp"
+#include <vector>
+#include <fstream>
 
 int main()
 {
@@ -8,15 +10,30 @@ int main()
     int user_input;
     std::cin >> user_input;
 
+    std::vector<int> prime_numbers;
     int prime_number_count = 0;
     int number = 2;
     while (prime_number_count < user_input)
     {
         if (is_prime_number(number))
         {
+            prime_numbers.push_back(number);
             prime_number_count++;
             // printf("%d: %d\n", prime_number_count, number);
         }
         number++;
     }
+
+    // for (int prime: prime_numbers)
+    // {
+    //     std::cout << prime << "\n";
+    // }
+
+    std::ofstream file;
+    file.open("prime_numbers.txt");
+    for (int prime_number : prime_numbers)
+    {
+        file << prime_number << std::endl;
+    }
+    file.close();
 }
